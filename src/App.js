526c22620b1ed68fce3ec5a6e9456247f2787f3c
dekimioc <dekimioc@yahoo.com>
2019-09-26 23:15:54 +0200

    Finished homepage and destroyed into really small component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import  HomePage  from './pages/homepage/HomePage';
+
 function App() {
   return (
-    <div className="App">
-      <h1>E-commerce with React</h1>
+    <div>
+      <HomePage />
     </div>
   );
 }
