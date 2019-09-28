e89da1fa8986e95eb0159b756981cb152d9aef0f
dekimioc <dekimioc@yahoo.com>
2019-09-28 10:15:54 +0200

    Update oue menu item component to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import  HomePage  from './pages/homepage/HomePage';
+
+const HatPage = props => {
+  console.log(props)
+  return (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
