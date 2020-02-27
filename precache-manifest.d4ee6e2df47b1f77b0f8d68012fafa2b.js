398e2eb6be0f2593cc828b52a67eaca35fcd9040
Dejan Miocinovic <dekimioc@yahoo.com>
2020-02-27 22:02:36 +0100

    Updates

---
+++ precache-manifest.d4ee6e2df47b1f77b0f8d68012fafa2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55251b4c1d9778f9649cb8857d54b0d1",
+    "url": "/e-commerce-with-React/index.html"
+  },
+  {
+    "revision": "df34bc0be6fb1f3d74bf",
+    "url": "/e-commerce-with-React/static/css/main.2ac6c7ef.chunk.css"
+  },
+  {
+    "revision": "d754fb9e05a15975ab0d",
+    "url": "/e-commerce-with-React/static/js/2.bce005a1.chunk.js"
+  },
+  {
+    "revision": "df34bc0be6fb1f3d74bf",
+    "url": "/e-commerce-with-React/static/js/main.8666ac34.chunk.js"
+  },
+  {
+    "revision": "d330be7c8b8e1357991a",
+    "url": "/e-commerce-with-React/static/js/runtime-main.9a8da08d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-with-React/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce-with-React/static/media/shopping-bag.30f07071.svg"
+  }
+]);
